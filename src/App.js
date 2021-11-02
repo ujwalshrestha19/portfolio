120a959dbf4df49f4b66e2bb5ea790aa496ee998
ujwalshrestha19 <ujwalshrestha19@gmail.com>
2021-11-02 20:22:44 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Testimonials from "./Components/Testimonials";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+
+    </main>
+  );
+}
+
+export default App;
